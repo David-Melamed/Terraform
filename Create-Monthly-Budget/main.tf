871d4fdeba06dfee4c9d7853af05bfd2682f1fb8
David-Melamed <davidmelamed269@gmail.com>
2022-08-09 12:02:17 +0300

    Budget creation v0.1

---
+++ Create-Monthly-Budget/main.tf
@@ -0,0 +1,30 @@
+﻿terraform {
+  cloud {
+    organization = "David-DevOps-learning-path"
+
+    workspaces {
+      name = "provisioners"
+    }
+  }
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "4.19.0"
+      }
+    }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  }
+
+resource "aws_budgets_budget" "monthly_cost_budget" {
+  name = "monthly-budget"
+  budget_type = "COST"
+  limit_amount = "100"
+  limit_unit = "USD"
+  time_unit = "MONTHLY"
+  time_period_start = "2022-06-23_00:00"
+}
